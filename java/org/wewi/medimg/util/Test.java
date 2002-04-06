5b91fe1ccf78956ee797c47cbfeca1c56e512daf
Franz Wilhelmstötter <franz.wilhelmstoetter@gmail.com>
2002-04-06 02:19:22 +0000

    no message

--- java/org/wewi/medimg/util/Test.java
+++ java/org/wewi/medimg/util/Test.java
@@ -24,6 +24,7 @@ public class Test {
     public static void main(String[] args) {
         Properties prop = System.getProperties();
         prop.list(System.out);
+        //
         
     }
     
