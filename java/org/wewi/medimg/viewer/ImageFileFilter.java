5867582f4e368e3c53bb2d8fad7fe41c609c026a
Franz Wilhelmstötter <franz.wilhelmstoetter@gmail.com>
2002-04-09 02:39:16 +0000

    no message

---
+++ java/org/wewi/medimg/viewer/ImageFileFilter.java
@@ -0,0 +1,48 @@
+/*
+ * ImageFileFilter.java
+ *
+ * Created on 8. April 2002, 18:27
+ */
+
+package org.wewi.medimg.viewer;
+
+import org.wewi.medimg.image.ImageFormatTypes;
+
+import java.io.File;
+
+import javax.swing.filechooser.FileFilter;
+
+/**
+ *
+ * @author  Franz Wilhelmstötter
+ * @version 0.1
+ */
+public class ImageFileFilter extends FileFilter {
+        private ImageFormatTypes type;
+        
+        public ImageFileFilter(ImageFormatTypes type) {
+            this.type = type;
+        }
+        
+        public boolean accept(File file) {
+            if (type.equals(ImageFormatTypes.TIFF_IMAGES)) {
+                return file.isDirectory();
+            } else if (type.equals(ImageFormatTypes.BMP_IMAGES)) {
+                return file.isDirectory();
+            } else if (type.equals(ImageFormatTypes.RAW_IMAGE)) {
+                if (file.isDirectory()) {
+                    return true;
+                }
+                return file.toString().endsWith(".rid");
+            }
+            return false;
+        }
+        
+        public String getDescription() {
+            return type.toString();
+        }
+ 
+        public ImageFormatTypes getImageFormatType() {
+            return type;
+        }
+}
