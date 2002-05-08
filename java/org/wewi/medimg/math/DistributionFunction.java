7234ec5e398e6c00de3b5c70247e4574257fb5f9
Franz Wilhelmstötter <franz.wilhelmstoetter@gmail.com>
2002-05-08 21:29:04 +0000

    no message

---
+++ java/org/wewi/medimg/math/DistributionFunction.java
@@ -0,0 +1,16 @@
+/*
+ * DistributionFunction.java
+ *
+ * Created on 11. April 2002, 10:17
+ */
+
+package org.wewi.medimg.math;
+
+/**
+ *
+ * @author  Franz Wilhelmstötter
+ * @version 0.1
+ */
+public interface DistributionFunction {
+    
+}
