b49a5dded3cff5f660f6d6e22d9ebcc57bbb1e2b
Franz Wilhelmstötter <franz.wilhelmstoetter@gmail.com>
2002-05-08 21:55:15 +0000

    no message

---
+++ java/org/wewi/medimg/QualityMeasure.java
@@ -0,0 +1,15 @@
+/*
+ * Quality.java
+ *
+ * Created on 8. Mai 2002, 13:32
+ */
+
+package org.wewi.medimg;
+
+/**
+ *
+ * @author  Franz Wilhelmstötter
+ */
+public interface QualityMeasure {
+    public double quality(); 
+}
