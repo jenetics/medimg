ef13655fe609dac02b34a3a3a7eb6b00de048b36
cvsuser <>
2002-05-11 04:38:36 +0000

    no message

---
+++ java/org/wewi/medimg/math/RealFunction.java
@@ -0,0 +1,16 @@
+/*
+ * RealFunction.java
+ *
+ * Created on 10. Mai 2002, 21:26
+ */
+
+package org.wewi.medimg.math;
+
+/**
+ *
+ * @author  Franz Wilhelmstötter
+ * @version 0.1
+ */
+public interface RealFunction extends Function {
+    public double eval(double arg);
+}
