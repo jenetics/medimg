d2cffcb86851fed45b74cced691cc52f081bb381
Franz Wilhelmstötter <franz.wilhelmstoetter@gmail.com>
2002-06-04 05:26:58 +0000

    no message

---
+++ java/org/wewi/medimg/seg/NullModelBasedSegmentation.java
@@ -0,0 +1,35 @@
+/*
+ * NullModelBasedSegmentation.java
+ *
+ * Created on 3. Juni 2002, 22:24
+ */
+
+package org.wewi.medimg.seg;
+
+import org.wewi.medimg.image.Image;
+
+import org.wewi.medimg.util.Nullable;
+
+/**
+ *
+ * @author  Franz Wilhelmstötter
+ * @version 0.1
+ */
+public class NullModelBasedSegmentation implements ModelBasedSegmentation,
+                                                   Nullable {
+    
+    /** Creates a new instance of NullModelBasedSegmentation */
+    public NullModelBasedSegmentation() {
+    }
+    
+    public void segmentate(Image sourceTarget) {
+    }
+    
+    public void segmentate(Image source, Image target) {
+    }
+    
+    public boolean isNull() {
+        return true;
+    }
+    
+}
