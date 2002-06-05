e23b29081b1e64f4096953a7b25b8383dbb53300
cvsuser <>
2002-06-05 03:00:39 +0000

    Ändern der Klasse auf ein Interface

--- java/org/wewi/medimg/image/ColorConversion.java
+++ java/org/wewi/medimg/image/ColorConversion.java
@@ -11,8 +11,8 @@ package org.wewi.medimg.image;
  * @author  Franz Wilhelmstötter
  * @version 0.2
  */
-public abstract class ColorConversion {
-    public abstract void convert(int grey, int[] rgb);
+public interface ColorConversion {
+    public void convert(int grey, int[] rgb);
 
-    public abstract int convert(int[] rgb);
+    public int convert(int[] rgb);
 }
