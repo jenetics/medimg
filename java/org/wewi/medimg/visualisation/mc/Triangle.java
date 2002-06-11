daef75d5846918bdcba35a3a28b08cb568724d1a
cvsuser <>
2002-06-11 00:05:46 +0000

    no message

---
+++ java/org/wewi/medimg/visualisation/mc/Triangle.java
@@ -0,0 +1,19 @@
+/*
+ * Triangle.java
+ *
+ * Created on 10. Juni 2002, 17:05
+ */
+
+package org.wewi.medimg.visualisation.mc;
+
+/**
+ *
+ * @author  Franz Wilhelmstötter
+ */
+public class Triangle {
+    
+    /** Creates a new instance of Triangle */
+    public Triangle() {
+    }
+    
+}
