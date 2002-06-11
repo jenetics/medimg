d32f1d372f2fe38d0b543f595e14edba8c4c590e
cvsuser <>
2002-06-11 00:43:15 +0000

    no message

---
+++ java/org/wewi/medimg/util/NullIterator.java
@@ -0,0 +1,32 @@
+/*
+ * NullIterator.java
+ *
+ * Created on 10. Juni 2002, 17:40
+ */
+
+package org.wewi.medimg.util;
+
+import java.util.Iterator;
+
+/**
+ *
+ * @author  Franz Wilhelmstötter
+ */
+public class NullIterator implements Iterator {
+    
+    /** Creates a new instance of NullIterator */
+    public NullIterator() {
+    }
+    
+    public boolean hasNext() {
+        return false;
+    }
+    
+    public Object next() {
+        return null;
+    }
+    
+    public void remove() {
+    }
+    
+}
