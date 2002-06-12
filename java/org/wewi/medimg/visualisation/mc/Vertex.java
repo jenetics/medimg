f35a9c43791e6cc4a23937f58bf49c069385a4ee
cvsuser <>
2002-06-12 03:22:53 +0000

    no message

---
+++ java/org/wewi/medimg/visualisation/mc/Vertex.java
@@ -0,0 +1,23 @@
+/*
+ * Vertex.java
+ *
+ * Created on 10. Juni 2002, 16:54
+ */
+
+package org.wewi.medimg.visualisation.mc;
+
+/**
+ *
+ * @author  Franz Wilhelmstötter
+ */
+public class Vertex extends Point {
+    
+    /** Creates a new instance of Vertex */
+    Vertex() {
+    }
+    
+    public Vertex(float x, float y, float z) {
+        super(x, y, z);
+    }
+    
+}
