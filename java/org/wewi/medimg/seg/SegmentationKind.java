f6ddb130646b44ad7c81f8d43ae858c9529776c8
Franz Wilhelmstötter <franz.wilhelmstoetter@gmail.com>
2002-06-12 04:45:40 +0000

    Hinzufügen der K_MEANS und MCCV Segmentierungsmethode.

--- java/org/wewi/medimg/seg/SegmentationKind.java
+++ java/org/wewi/medimg/seg/SegmentationKind.java
@@ -16,6 +16,8 @@ import org.wewi.medimg.util.Enumeration;
 public class SegmentationKind extends Enumeration {
     public static final SegmentationKind ML = new SegmentationKind();
     public static final SegmentationKind MAP = new SegmentationKind();
+    public static final SegmentationKind K_MEANS = new SegmentationKind();
+    public static final SegmentationKind MCCV = new SegmentationKind();
     
     private static int refCount = 0;  
     private SegmentationKind() { 
