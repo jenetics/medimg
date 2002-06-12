1759f4fdec3b58687ed8b15fe2e3acc74799c736
Franz Wilhelmstötter <franz.wilhelmstoetter@gmail.com>
2002-06-12 23:48:18 +0000

    no message

---
+++ java/org/wewi/medimg/visualisation/mc/TriangleDecimator.java
@@ -0,0 +1,19 @@
+/*
+ * TriangleDecimator.java
+ *
+ * Created on 12. Juni 2002, 15:41
+ */
+
+package org.wewi.medimg.visualisation.mc;
+
+/**
+ *
+ * @author  werner weiser
+ * @version 
+ */
+public interface TriangleDecimator {
+    
+    public void decimate(Graph graph); 
+
+}
+
