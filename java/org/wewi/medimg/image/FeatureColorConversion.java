cbf6f69e8a67e91fce17cd71eefb0415eefebdb4
Franz Wilhelmstötter <franz.wilhelmstoetter@gmail.com>
2002-08-07 14:21:30 +0000

    no message

--- java/org/wewi/medimg/image/FeatureColorConversion.java
+++ java/org/wewi/medimg/image/FeatureColorConversion.java
@@ -6,21 +6,36 @@
 
 package org.wewi.medimg.image;
 
+import java.awt.Color;
+
 /**
  *
  * @author  Franz Wilhelmstötter
  * @version 0.1
  */
 public class FeatureColorConversion implements ColorConversion {
-    private static final int[][] cc = {{255, 255, 255},
-                                       {255, 0, 0},
-                                       {0, 255, 0},
-                                       {0, 0, 255},
-                                       {255, 255, 0},
-                                       {0, 255, 255},
-                                       {0, 0, 0}};
+    private static final Color[] c = {Color.white,
+                                          Color.red,
+                                          Color.green,
+                                          Color.blue,
+                                          Color.orange,
+                                          Color.pink,
+                                          Color.yellow,
+                                          Color.cyan,
+                                          Color.lightGray,
+                                          Color.gray,
+                                          Color.darkGray,
+                                          Color.black};
+    private static final int colors = c.length;
+    private static int[][] cc;
 
     public FeatureColorConversion() {
+        cc = new int[colors][3];
+        for (int i = 0; i < colors; i++) {
+            cc[i][0] = c[i].getRed();
+            cc[i][1] = c[i].getGreen();
+            cc[i][2] = c[i].getBlue();
+        }
     }
 
     public void convert(int grey, int[] rgb) {
