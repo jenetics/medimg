a3c34032835ed8d5e372237c7d2e7736a191cd39
Franz Wilhelmstötter <franz.wilhelmstoetter@gmail.com>
2002-08-17 13:16:47 +0000

    *** empty log message ***

---
+++ java/org/wewi/medimg/seg/stat/IterationRecord.java
@@ -0,0 +1,24 @@
+/**
+ * Created on 14.08.2002
+ *
+ * To change this generated comment edit the template variable "filecomment":
+ * Window>Preferences>Java>Templates.
+ * To enable and disable the creation of file comments go to
+ * Window>Preferences>Java>Code Generation.
+ */
+package org.wewi.medimg.seg.stat;
+
+/**
+ * @author Franz Wilhelmstötter
+ *
+ * To change this generated comment edit the template variable "typecomment":
+ * Window>Preferences>Java>Templates.
+ * To enable and disable the creation of type comments go to
+ * Window>Preferences>Java>Code Generation.
+ */
+public interface IterationRecord {
+	public String toString();
+	
+	public String toXMLString();
+
+}
