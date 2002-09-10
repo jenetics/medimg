531891a1e51bed635abb916d492df51b4f37b425
Franz Wilhelmstötter <franz.wilhelmstoetter@gmail.com>
2002-09-10 19:38:48 +0000

    *** empty log message ***

---
+++ java/org/wewi/medimg/alg/IterationEvent.java
@@ -0,0 +1,26 @@
+/**
+ * Created on 10.09.2002
+ *
+ * To change this generated comment edit the template variable "filecomment":
+ * Window>Preferences>Java>Templates.
+ * To enable and disable the creation of file comments go to
+ * Window>Preferences>Java>Code Generation.
+ */
+package org.wewi.medimg.alg;
+
+import java.util.EventObject;
+
+/**
+ * @author Franz Wilhelmstötter
+ * @version 0.1
+ *
+ * To change this generated comment edit the template variable "typecomment":
+ * Window>Preferences>Java>Templates.
+ * To enable and disable the creation of type comments go to
+ * Window>Preferences>Java>Code Generation.
+ */
+public class IterationEvent extends EventObject {
+    public IterationEvent(Object source) {
+        super(source);    
+    }
+}
