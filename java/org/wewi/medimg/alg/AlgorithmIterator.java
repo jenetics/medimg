d76e84e5c60dc7f5cffd4f171e0fd2832770b0bf
Franz Wilhelmstötter <franz.wilhelmstoetter@gmail.com>
2002-09-10 17:57:20 +0000

    *** empty log message ***

---
+++ java/org/wewi/medimg/alg/AlgorithmIterator.java
@@ -0,0 +1,27 @@
+/**
+ * Created on 31.08.2002
+ *
+ * To change this generated comment edit the template variable "filecomment":
+ * Window>Preferences>Java>Templates.
+ * To enable and disable the creation of file comments go to
+ * Window>Preferences>Java>Code Generation.
+ */
+package org.wewi.medimg.alg;
+
+/**
+ * @author Franz Wilhelmstötter
+ * @version 0.1
+ *
+ * To change this generated comment edit the template variable "typecomment":
+ * Window>Preferences>Java>Templates.
+ * To enable and disable the creation of type comments go to
+ * Window>Preferences>Java>Code Generation.
+ */
+public interface AlgorithmIterator {
+    
+    public boolean hasNextIteration();
+    
+    public void nextIteration();
+    
+    public Object getInterimResult() throws UnsupportedOperationException;
+}
