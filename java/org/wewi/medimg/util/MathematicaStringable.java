8d8f8fa660440f989359f7f152cff428d3a8b3ca
cvsuser <>
2002-09-18 11:37:57 +0000

    *** empty log message ***

---
+++ java/org/wewi/medimg/util/MathematicaStringable.java
@@ -0,0 +1,21 @@
+/**
+ * Created on 18.09.2002
+ *
+ */
+package org.wewi.medimg.util;
+
+/**
+ * @author Franz Wilhelmstötter
+ * @version 0.1
+ * 
+ * Wird dieses Interface implementiert, so bedeutet dies,
+ * daß der gelieferte String eine Darstellung
+ * des Objektes in Mathematica ermöglicht. Dies
+ * kann ein Diagramm sein, eine Graphik oder
+ * auch nur die Darstellung der Datenstruktur
+ * in der Syntax von Mathematica.
+ *
+ */
+public interface MathematicaStringable {
+    public String toMathematicaString();
+}
