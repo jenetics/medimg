4e015ca349e331f5d9963ad0219c572a3e90213d
cvsuser <>
2002-09-24 21:19:18 +0000

    *** empty log message ***

---
+++ java/org/wewi/medimg/image/geom/transform/InterpolateableTransformation.java
@@ -0,0 +1,15 @@
+/**
+ * Created on 24.09.2002
+ *
+ */
+package org.wewi.medimg.image.geom.transform;
+
+/**
+ * @author Franz Wilhelmstötter
+ * @author Werner Weiser
+ * 
+ * @version 0.1
+ */
+public interface InterpolateableTransformation extends Interpolateable, Transformation {
+
+}
