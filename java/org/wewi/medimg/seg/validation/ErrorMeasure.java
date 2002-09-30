ef1dce14c377f7aa91cff00eadcfd9cc9e70da8b
Franz Wilhelmstötter <franz.wilhelmstoetter@gmail.com>
2002-09-30 09:08:52 +0000

    *** empty log message ***

---
+++ java/org/wewi/medimg/seg/validation/ErrorMeasure.java
@@ -0,0 +1,75 @@
+/**
+ * Created on 30.09.2002
+ *
+ */
+package org.wewi.medimg.seg.validation;
+
+import java.util.Arrays;
+
+import org.wewi.medimg.image.Image;
+
+/**
+ * @author Franz Wilhelmstötter
+ * @version 0.1
+ */
+public class ErrorMeasure {
+    private Image am;
+    private Image s;
+    private T3 t3;
+    
+    private double[] featureError;
+    private double overallError;
+
+	/**
+	 * Constructor for ErrorMeasure.
+	 */
+	public ErrorMeasure(Image am, Image s, T3 t3) {
+		super();
+        this.am = am;
+        this.s = s;
+        this.t3 = t3;
+        
+        featureError = new double[t3.getAbstractFeatures()];
+        overallError = 0;
+        Arrays.fill(featureError, 0);
+	}
+    
+    public void measure() {
+        int[] abstractFeatureVoxels = new int[t3.getAbstractFeatures()];
+        int[] errorVoxels = new int[t3.getAbstractFeatures()];
+        Arrays.fill(abstractFeatureVoxels, 0);
+        Arrays.fill(errorVoxels, 0);
+        
+        
+        int af = 0, mf = 0;
+        for (int i = 0, n = am.getNVoxels(); i < n; i++) {
+            mf = am.getColor(i);
+            af = s.getColor(i);
+            ++abstractFeatureVoxels[af];
+            
+            if (mf != t3.transform(af)) {
+                ++errorVoxels[af];    
+            }        
+        } 
+        
+        for (int i = 0, n = t3.getAbstractFeatures(); i < n; i++) {
+            featureError[i] = (double)errorVoxels[i] / (double)abstractFeatureVoxels[i];    
+        }  
+        
+        int errorSum = 0;
+        for (int i = 0; i < errorVoxels.length; i++) {
+            errorSum += errorVoxels[i];    
+        }
+        
+        overallError = (double)errorSum / (double)am.getNVoxels();
+    }
+    
+    public double[] getFeatureError() {
+        return featureError;    
+    }
+    
+    public double getOverallError() {
+        return overallError;    
+    }
+
+}
--- java/org/wewi/medimg/seg/validation/Util.java
+++ java/org/wewi/medimg/seg/validation/Util.java
@@ -4,7 +4,12 @@
  */
 package org.wewi.medimg.seg.validation;
 
+import java.util.Enumeration;
+import java.util.Properties;
+
+import org.jdom.Attribute;
 import org.jdom.Element;
+import org.wewi.medimg.image.Image;
 
 /**
  * @author Franz Wilhelmstötter
@@ -18,14 +23,95 @@ final class Util {
     static Element transform(AccumulatorArray accu) {
         Element table = new Element("FrequencyMatrix");
         
+        Element td, tr;
         for (int i = 0, n = accu.getRows(); i < n; i++) {
+            tr = new Element("Row");
             for (int j = 0, m = accu.getCols(); j < m; j++) {
-            
-            }       
+                td = new Element("ColData");
+                td.addContent(Integer.toString(accu.getValue(i, j)));
+                tr.addContent(td);   
+            }
+            table.addContent(tr);       
         }
         
         
+        return table;    
+    }
+    
+    static Element transform(T3 t3) {
+        Element transform = new Element("T3");
+        Element map;
+        for (int i = 0, n = t3.getAbstractFeatures(); i < n; i++) {
+            map = new Element("Map");
+            map.setAttribute(new Attribute("source", Integer.toString(i))); 
+            map.setAttribute(new Attribute("target", Integer.toString(t3.transform(i)))); 
+            transform.addContent(map);      
+        } 
+        
+        return transform;   
+    }
+    
+    static Element transform(ErrorMeasure em) {
+        Element error = new Element("Error");
+        double[] featureError = em.getFeatureError();
+        double overallError = em.getOverallError();
+        
+        Element oe = new Element("OverallError");
+        oe.addContent(Double.toString(overallError));
+        error.addContent(oe);
+        
+        Element fe;
+        for (int i = 0; i < featureError.length; i++) {
+            fe = new Element("FeatureError");
+            fe.setAttribute(new Attribute("feature", Integer.toString(i)));
+            fe.addContent(Double.toString(featureError[i])); 
+            error.addContent(fe);   
+        }
+        
+        return error;    
+    }
+    
+    static Element transform(Image image) {
+        Element imageMetaData = new Element("ImageMetaData"); 
+        Properties prop = image.getHeader().getImageProperties();
+        
+        for (Enumeration e = prop.propertyNames(); e.hasMoreElements();) {
+            String name = (String)e.nextElement();
+            imageMetaData.addContent((new Element(name)).addContent(prop.getProperty(name)));
+        }
+        
+        return imageMetaData;   
+    }
+    
+    static Element transform(Object o) {
         return null;    
     }
 
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
