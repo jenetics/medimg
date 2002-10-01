89fcb8d1196cbf1db2c8331053ba9455631bdb82
cvsuser <>
2002-10-01 09:52:24 +0000

    *** empty log message ***

---
+++ java/org/wewi/medimg/util/MutableDouble.java
@@ -0,0 +1,34 @@
+/**
+ * Created on 01.10.2002
+ *
+ */
+package org.wewi.medimg.util;
+
+/**
+ * @author Franz Wilhelmstötter
+ * @version 0.1
+ */
+public class MutableDouble implements Mutable {
+    private double value;
+
+	/**
+	 * Constructor for MutableDouble.
+	 */
+	public MutableDouble(double value) {
+		this.value = value;
+	}
+    
+    public MutableDouble() {
+        this(0);    
+    }
+    
+    
+    public void setValue(double value) {
+        this.value = value;    
+    }
+    
+    public double getValue() {
+        return value;   
+    }
+
+}
