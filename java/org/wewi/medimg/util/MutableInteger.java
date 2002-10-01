777a251504732794508d13bf0cdf7e521228205d
Franz Wilhelmstötter <franz.wilhelmstoetter@gmail.com>
2002-10-01 09:52:24 +0000

    *** empty log message ***

---
+++ java/org/wewi/medimg/util/MutableInteger.java
@@ -0,0 +1,35 @@
+/**
+ * Created on 01.10.2002
+ *
+ */
+package org.wewi.medimg.util;
+
+
+
+/**
+ * @author Franz Wilhelmstötter
+ * @version 0.1
+ */
+public class MutableInteger implements Mutable {
+    private int value;
+
+	/**
+	 * Constructor for MutableInteger.
+	 */
+	public MutableInteger(int value) {
+        this.value = value;
+	}
+    
+    public MutableInteger() {
+        this(0);
+    }
+    
+    public void setValue(int value) {
+        this.value = value;    
+    }
+    
+    public int getValue() {
+        return value;    
+    }
+
+}
