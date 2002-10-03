88266c49a439c4bf5f8df8794a0a7742d42a864f
Franz Wilhelmstötter <franz.wilhelmstoetter@gmail.com>
2002-10-03 11:48:14 +0000

    *** empty log message ***

---
+++ java/org/wewi/medimg/image/io/JPEGReaderFactory.java
@@ -0,0 +1,27 @@
+package org.wewi.medimg.image.io;
+
+import java.io.File;
+
+import org.wewi.medimg.image.ImageFactory;
+
+/**
+ * @author Franz Wilhelmstötter
+ * @version 0.1
+ */
+public class JPEGReaderFactory implements ImageReaderFactory {
+    private Range range;
+
+    public JPEGReaderFactory() {
+        range = new Range(0, Integer.MAX_VALUE, 1);
+    }
+    
+    public ImageReader createImageReader(ImageFactory imageFactory, File file) {
+        ImageReader reader = new JPEGReader(imageFactory, file, range);
+        return reader;
+    }
+    
+    public void setRange(Range range) {
+        this.range = range;
+    }
+
+}
