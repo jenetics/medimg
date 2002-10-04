82a646e5dc83fa324288757b74514043a17aa89a
cvsuser <>
2002-10-04 11:25:24 +0000

    *** empty log message ***

---
+++ java/org/wewi/medimg/reg/ConstantTransformationImportance.java
@@ -0,0 +1,34 @@
+package org.wewi.medimg.reg;
+
+import java.util.Arrays;
+
+
+/**
+ * @author Franz Wilhelmstötter
+ * @author Werner Weiser
+ * 
+ * @version 0.1  
+ */
+public class ConstantTransformationImportance implements TransformationImportance {
+    public static final ConstantTransformationImportance INSTANCE = new ConstantTransformationImportance();
+
+	/**
+	 * Constructor for ConstantTransformationImportance.
+	 */
+	public ConstantTransformationImportance() {
+		super();
+	}
+
+	/**
+
+	/**
+	 * @see org.wewi.medimg.reg.TransformationImportance#transformationWeights(int[], double[], int[])
+	 */
+	public double[] transformationWeights(int[] features, double[] similarity, int[] featureNPoints) {
+		double[] result = new double[features.length];
+        Arrays.fill(result, 1d/(double)features.length);
+        return result;
+	}
+    
+}
+
