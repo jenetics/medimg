394c2f6a872149d61c2632d396da5dbc86e5b230
Franz Wilhelmstötter <franz.wilhelmstoetter@gmail.com>
2002-10-11 16:58:10 +0000

    *** empty log message ***

--- java/viewer.cmd
+++ java/viewer.cmd
@@ -1,3 +1,7 @@
 c:
 cd C:\Workspace\fwilhelm\Projekte\Diplom\code\java
-java -da -Xmx256m org.wewi.medimg.viewer.Viewer %1
+
+set LOCALCLASSPATH=.;..\lib\colt.jar;..\lib\jdom.jar;..\lib\junit.jar;..\lib\xalan.jar;..\lib\xerces.jar
+set LOCALCLASSPATH=%LOCALCLASSPATH%;..\lib\jai_codec.jar;..\lib\jai_core.jar;..\lib\mlibwrapper_jai.jar
+
+java -da -Xmx256m -classpath %LOCALCLASSPATH% org.wewi.medimg.viewer.Viewer %1
