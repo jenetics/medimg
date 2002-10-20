4d4d46ef2815f360d210ea4828e3278e9b18027b
cvsuser <>
2002-10-20 12:56:00 +0000

    *** empty log message ***

---
+++ java/org/wewi/medimg/alg/IterateableAlgorithm.java
@@ -0,0 +1,13 @@
+/**
+ * Created on 20.10.2002 14:55:06
+ *
+ */
+package org.wewi.medimg.alg;
+
+/**
+ * @author Franz Wilhelmstötter
+ * @version 0.1
+ */
+public interface IterateableAlgorithm {
+    public AlgorithmIterator getAlgorithmIterator();
+}
