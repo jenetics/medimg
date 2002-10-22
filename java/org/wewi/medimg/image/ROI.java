1e11439f1b35ad6e6b4499b1079ac461b8cae06f
cvsuser <>
2002-10-22 18:28:11 +0000

    *** empty log message ***

---
+++ java/org/wewi/medimg/image/ROI.java
@@ -0,0 +1,35 @@
+/**
+ * Created on 22.10.2002 20:26:54
+ *
+ */
+package org.wewi.medimg.image;
+
+/**
+ * @author Franz Wilhelmstötter
+ * @version 0.1
+ */
+public class ROI extends Dimension {
+
+	/**
+	 * Constructor for ROI.
+	 * @param dim
+	 */
+	public ROI(ROI dim) {
+		super(dim);
+	}
+
+	/**
+	 * Constructor for ROI.
+	 * @param minX
+	 * @param maxX
+	 * @param minY
+	 * @param maxY
+	 * @param minZ
+	 * @param maxZ
+	 * @throws IllegalArgumentException
+	 */
+	public ROI(int minX, int maxX, int minY, int maxY, int minZ, int maxZ) throws IllegalArgumentException {
+		super(minX, maxX, minY, maxY, minZ, maxZ);
+	}
+
+}
