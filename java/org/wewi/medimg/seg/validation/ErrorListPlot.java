fdcb407f2c7fab56e703dfe7a7b0ffe921e3c050
cvsuser <>
2002-10-28 15:55:17 +0000

    *** empty log message ***

--- java/org/wewi/medimg/seg/validation/ErrorListPlot.java
+++ java/org/wewi/medimg/seg/validation/ErrorListPlot.java
@@ -7,27 +7,19 @@
 package org.wewi.medimg.seg.validation;
 
 import java.awt.Color;
-import java.awt.Font;
 
-import com.jrefinery.chart.ChartFactory;
 import com.jrefinery.chart.ChartPanel;
 import com.jrefinery.chart.HorizontalCategoryAxis;
 import com.jrefinery.chart.JFreeChart;
 import com.jrefinery.chart.LineAndShapeRenderer;
 import com.jrefinery.chart.OverlaidVerticalCategoryPlot;
 import com.jrefinery.chart.SeriesShapeFactory;
-import com.jrefinery.chart.StandardXYItemRenderer;
 import com.jrefinery.chart.VerticalCategoryPlot;
 import com.jrefinery.chart.VerticalIntervalBarRenderer;
 import com.jrefinery.chart.VerticalNumberAxis;
-import com.jrefinery.chart.XYPlot;
 import com.jrefinery.data.DefaultCategoryDataset;
 import com.jrefinery.data.DefaultIntervalCategoryDataset;
-import com.jrefinery.data.DefaultStatisticalCategoryDataset;
-import com.jrefinery.data.DefaultXYDataset;
 import com.jrefinery.data.IntervalCategoryDataset;
-import com.jrefinery.data.StatisticalCategoryDataset;
-import com.jrefinery.data.XYDataset;
 
 /**
  *
@@ -58,57 +50,6 @@ public class ErrorListPlot extends javax.swing.JFrame {
     }
     
     public void init() { 
-        /*
-        Object[][][] data = new Object[1][k.length][2];
-        //Auffüllen des Datenobjektes
-        for (int i = 0; i < k.length; i++) {
-            data[0][i][0] = new Integer(k[i]);
-            data[0][i][1] = new Double(mean[i]);    
-        }
-        XYDataset dataSet = new DefaultXYDataset(data);
-        
-        freeChart = ChartFactory.createXYChart("Titel", "X-Achse", "Y-Achse", dataSet, false);
-        freeChart.setBackgroundPaint(Color.WHITE);
-        
-        chartPanel = new ChartPanel(freeChart);
-        getContentPane().add(chartPanel);
-        */
-        /*
-        double[][] m = new double[1][];
-        m[0] = mean;
-        double[][] v = new double[1][];
-        v[0] = var;
-        StatisticalCategoryDataset data = new DefaultStatisticalCategoryDataset(m, v);
-        freeChart = ChartFactory.createAreaChart("Titel", "x", "y", data, false);
-        freeChart.setBackgroundPaint(Color.WHITE);
-        chartPanel = new ChartPanel(freeChart);
-        getContentPane().add(chartPanel);   
-        */  
-        /*
-        double[][] starts = new double[1][mean.length];
-        double[][] ends = new double[1][mean.length];
-        for (int i =  0; i < mean.length; i++) {
-            starts[0][i] = mean[i] - var[i];
-            ends[0][i] = mean[i] + var[i];    
-        }
-        
-        IntervalCategoryDataset data = new DefaultIntervalCategoryDataset(starts, ends);
-        String title = "Anzahl der Merkmale Diagramm";
-        String xTitle = "Anzahl der Merkmale k";
-        String yTitle = "Relative Fehlerhäufigkeit";
-        HorizontalCategoryAxis xAxis = new HorizontalCategoryAxis(xTitle);
-        VerticalNumberAxis yAxis = new VerticalNumberAxis(yTitle);
-        VerticalIntervalBarRenderer renderer = new VerticalIntervalBarRenderer();
-        VerticalCategoryPlot plot = new VerticalCategoryPlot(data, xAxis, yAxis, renderer);
-        plot.setLabelFormatString("0.##%");
-        plot.setBackgroundPaint(Color.lightGray);
-        plot.setOutlinePaint(Color.white);
-        freeChart = new JFreeChart(title, JFreeChart.DEFAULT_TITLE_FONT, plot, false);       
-        freeChart.setBackgroundPaint(Color.WHITE);
-        chartPanel = new ChartPanel(freeChart);
-        getContentPane().add(chartPanel); 
-        */
-        
         //Erzeugen der Datensätze
         String[] categories = new String[mean.length];
         double[][] starts = new double[1][mean.length];
@@ -135,22 +76,17 @@ public class ErrorListPlot extends javax.swing.JFrame {
         
         VerticalIntervalBarRenderer renderer = new VerticalIntervalBarRenderer();
         VerticalCategoryPlot bar = new VerticalCategoryPlot(intervalData, null, null, renderer); 
-        //bar.setLabelsVisible(true);
-        //bar.setLabelFont(JFreeChart.DEFAULT_TITLE_FONT);
-        //bar.setLabelFormatString("0.##%");
         plot.add(bar);  
        
         LineAndShapeRenderer lineRenderer = null;
         lineRenderer = new LineAndShapeRenderer(LineAndShapeRenderer.SHAPES_AND_LINES);
         VerticalCategoryPlot lines = null;
         lines = new VerticalCategoryPlot(lineData, null, null, lineRenderer);
-        //lines.setSeriesPaint(lineColors);
+        lines.setSeriesPaint(new Color[]{Color.BLUE});
         lines.setShapeFactory(new SeriesShapeFactory());
         plot.add(lines); 
-        
-        lines.setParent(bar);
 
-        freeChart = new JFreeChart(title, JFreeChart.DEFAULT_TITLE_FONT, bar, false);       
+        freeChart = new JFreeChart(title, JFreeChart.DEFAULT_TITLE_FONT, plot, false);       
         freeChart.setBackgroundPaint(Color.WHITE);
         chartPanel = new ChartPanel(freeChart);
         getContentPane().add(chartPanel);                 
