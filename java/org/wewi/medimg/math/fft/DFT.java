8a4d24e1276382970acd6d403b996ff8e130b577
cvsuser <>
2002-12-14 14:36:52 +0000

    *** empty log message ***

---
+++ java/org/wewi/medimg/math/fft/DFT.java
@@ -0,0 +1,22 @@
+/**
+ * DFT.java
+ * 
+ * Created on 14.12.2002, 15:28:34
+ *
+ */
+package org.wewi.medimg.math.fft;
+
+/**
+ * @author Franz Wilhelmstötter
+ * @version 0.1
+ */
+public class DFT {
+
+    /**
+     * Constructor for DFT.
+     */
+    public DFT() {
+        super();
+    }
+
+}
