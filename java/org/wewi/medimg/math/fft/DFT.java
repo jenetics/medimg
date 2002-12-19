44a8b3aa58f562dcbb1cbc420b2e4bdb5ec1028b
Franz Wilhelmstötter <franz.wilhelmstoetter@gmail.com>
2002-12-19 22:15:58 +0000

    *** empty log message ***

--- java/org/wewi/medimg/math/fft/DFT.java
+++ java/org/wewi/medimg/math/fft/DFT.java
@@ -6,15 +6,14 @@
  */
 package org.wewi.medimg.math.fft;
 
-import org.wewi.medimg.math.Complex;
-import org.wewi.medimg.math.MathUtil;
-import org.wewi.medimg.util.Timer;
 
 /**
  * @author Franz Wilhelmstötter
  * @version 0.1
  */
-public class DFT {
+public abstract class DFT {
+    protected double alpha = 1;
+    protected double beta = -1;
 
     /**
      * Constructor for DFT.
@@ -22,127 +21,28 @@ public class DFT {
     public DFT() {
         super();
     }
-    
-    public boolean isPowerOfTwo(int n) {
-        double ldn = MathUtil.log2(n);
-        
-        return true;    
-    }    
-         
-    
-    
-    public static Complex[][] naiveFFT(Complex[][] a) {
-        DFT1D dft = new NaiveDFT1D();
-        
-        Complex[][] A = new Complex[a.length][a[0].length];
-        for (int i = 0; i < a.length; i++) {
-            for (int j = 0; j < a[0].length; j++) {
-                A[i][j] = a[i][j];    
-            }    
-        }
-        
-        //Zeilentransformation
-        for (int i = 0; i < a.length; i++) {
-            dft.transform(A[i]);   
-        }
-        
-        
-        //Spaltentransformation
-        Complex[] col = new Complex[A.length];
-        for (int i = 0; i < a[0].length; i++) {
-            for (int j = 0; j < a.length; j++) {
-                col[j] = A[j][i];    
-            }    
-            
-            dft.transform(col);
-            
-            for (int j = 0; j < a.length; j++) {
-                A[j][i] = col[j];    
-            }            
-        }
-        
-        
-        return A;    
-    } 
-    private static void scale(double s, Complex[] a) {
-        for (int i = 0; i < a.length; i++) {
-            a[i]  = MathUtil.mult(s, a[i]);    
-        }
+
+    public DFT(double alpha, double beta) {
+        this.alpha = alpha;
+        this.beta = beta;
     }
-    
-    
-    
-    public static void main(String[] args) {
-        /*
-        final int SIZE = (int)MathUtil.pow(2, 20);
-        Complex[] data = new Complex[SIZE];
-        Complex[] result;
-        for (int i = 0; i < SIZE; i++) {
-            data[i] = new Complex(Math.sin(i), Math.cos(i)+1);    
-        }
-        
-        Timer t = new Timer("FFT");
-        t.start();
-        //naiveDFT1D(data);
-        t.stop();
-        t.print();
-        
-        t.start();
-        recursiveFFT(data);
-        t.stop();
-        t.print();
-        
-        t.start();
-        iterativeFFT(data);
-        t.stop();
-        t.print();                
-        */
-        
-        
-        Complex[][] data = new Complex[][]{{new Complex(1), new Complex(2)},
-                                            {new Complex(2), new Complex(5)},
-                                            {new Complex(0), new Complex(0)},
-                                            {new Complex(0), new Complex(0)}};
-        Complex[][] result;
-        
-        result = naiveFFT(data);
-        
-        for (int i = 0; i < result.length; i++) {
-            for (int j = 0; j < result[0].length; j++) {
-                System.out.print(result[i][j]);
-                System.out.print("    ");    
-            }    
-            System.out.println();
-        }
-        
-/*
-        Complex[] data = new Complex[4];
-        data[0] = new Complex(1, 0);
-        data[1] = new Complex(4, 0);
-        data[2] = new Complex(2, 0);
-        data[3] = new Complex(6, 0);
-        Complex[] result;
-        
-        result = naiveDFT1D(data);
-        for (int i = 0; i < result.length; i++) {
-            System.out.println(result[i]);    
-        }
-        System.out.println();        
-        
-        result = recursiveFFT(data);
-        for (int i = 0; i < result.length; i++) {
-            System.out.println(result[i]);    
-        }
-        System.out.println();
-                
-        result = iterativeFFT(data);        
-        for (int i = 0; i < result.length; i++) {
-            System.out.println(result[i]);    
-        }
-*/                
 
+    protected boolean isPowerOfTwo(int n) {
+        int log2N = 0;
+        int k = 1;
 
-            
+        while (k < n) {
+            k <<= 1;
+            log2N++;
+        }
+
+        int ntest = (1 << log2N);
+
+        if (n != ntest) {
+            return false; // n is not a power of 2
+        }
+
+        return true;
     }
 
 }
--- java/org/wewi/medimg/math/fft/NaiveDFT1D.java
+++ java/org/wewi/medimg/math/fft/NaiveDFT1D.java
@@ -21,26 +21,54 @@ public final class NaiveDFT1D extends DFT implements DFT1D {
     public NaiveDFT1D() {
         super();
     }
-
-    /**
-     * @see org.wewi.medimg.math.fft.DFT1D#dft(Complex[])
-     */
-    public void transform(Complex[] a) {
-        final int N = a.length;
-        final Complex Wn = MathUtil.exp(new Complex(0, 2*Math.PI/(double)N));
+    
+    public NaiveDFT1D(double alpha, double beta) {
+        super(alpha, beta);    
+    }
+    
+    private void trans(Complex[] data, double dir) {
+        final int N = data.length;
+        final double M = 1d/(Math.pow((double)N, (1d - (dir*alpha))/2d));
+        final Complex Wn = MathUtil.exp(new Complex(0, dir*2*Math.PI*beta/(double)N));
         
-        Complex[] result = new Complex[N];                             
-        final double MULT = 1d/Math.sqrt(N);                                     
+        Complex[] result = new Complex[N];                                                                 
                                      
         for (int n = 0; n < N; n++) {
             Complex z = new Complex(0, 0);
             for (int k = 0; k < N; k++) {
-                z = z.add(MathUtil.mult(a[k], MathUtil.pow(Wn, new Complex(k*n))));      
-            }
-            result[n] = MathUtil.mult(MULT, z);    
+                z = z.add(MathUtil.mult(data[k], MathUtil.pow(Wn, new Complex(k*n))));      
+            } 
+            
+            result[n] = MathUtil.mult(M, z); 
         }
         
-        System.arraycopy(result, 0, a, 0, N);       
+        System.arraycopy(result, 0, data, 0, N);        
+    }
+
+    /**
+     * Fouriertransformation.
+     * 
+     * <code>
+     *     F(n) = Sum[f(k)*exp[-2*PI*n*k/N], {k, 0, N-1}]
+     * </code>
+     * 
+     * @see org.wewi.medimg.math.fft.DFT1D#dft(Complex[])
+     */
+    public void transform(Complex[] data) {
+        trans(data, +1);
+    }
+
+    /**
+     * Inverse Fouriertransformation
+     * 
+     * <code>
+     *     f(n) = (1/N)*Sum[F(k)*exp[2*PI*n*k/N], {k, 0, N-1}]
+     * </code>
+     * 
+     * @see org.wewi.medimg.math.fft.DFT1D#transformBackward(Complex[])
+     */
+    public void transformInverse(Complex[] a) {
+        trans(a, -1);
     }
 
 }
