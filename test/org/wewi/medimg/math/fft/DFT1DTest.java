f961536aebb932ad515ddd26082c877aa02c28aa
cvsuser <>
2002-12-19 11:48:13 +0000

    *** empty log message ***

---
+++ test/org/wewi/medimg/math/fft/DFT1DTest.java
@@ -0,0 +1,39 @@
+/**
+ * DFT1DTest.java
+ * 
+ * Created on 19.12.2002, 12:47:33
+ *
+ */
+package org.wewi.medimg.math.fft;
+
+import junit.framework.TestCase;
+
+/**
+ * @author Franz Wilhelmstötter
+ * @version 0.1
+ */
+public class DFT1DTest extends TestCase {
+
+    /**
+     * Constructor for DFT1DTest.
+     * @param arg0
+     */
+    public DFT1DTest(String arg0) {
+        super(arg0);
+    }
+
+    /**
+     * @see TestCase#setUp()
+     */
+    protected void setUp() throws Exception {
+        super.setUp();
+    }
+
+    /**
+     * @see TestCase#tearDown()
+     */
+    protected void tearDown() throws Exception {
+        super.tearDown();
+    }
+
+}
