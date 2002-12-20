c389a66a7f67ef3ae964e287191d3d8be2630076
Franz Wilhelmstötter <franz.wilhelmstoetter@gmail.com>
2002-12-20 14:32:16 +0000

    *** empty log message ***

--- java/org/wewi/medimg/math/fft/DFT2D.java
+++ java/org/wewi/medimg/math/fft/DFT2D.java
@@ -21,36 +21,57 @@ public class DFT2D extends DFT {
     public DFT2D() {
         this(new IterativeFFT1D());
     }
-    
+
     public DFT2D(DFT1D ft) {
-        this.ft = ft;    
+        this.ft = ft;
     }
-    
+
     /**
      * In Place Transformation.
      * 
      * @param a Komplexes Eingabearray.
-     */ 
-     public void transform(Complex[][] a) {
+     */
+    public void transform(Complex[][] data) {
+        transform(data, new TransformFunction() {
+                            public void transform(Complex[] data) {
+                                ft.transform(data);    
+                            }    
+                        });
+    }
+
+    /**
+     * In Place Transformation.
+     * 
+     * @param a Komplexes Eingabearray.
+     */
+    public void transformInverse(Complex[][] data) {
+        transform(data, new TransformFunction() {
+                            public void transform(Complex[] data) {
+                                ft.transformInverse(data);    
+                            }    
+                        });
+    }
+
+
+    private void transform(Complex[][] data, TransformFunction f) {
         //Zeilentransformation
-        for (int i = 0; i < a.length; i++) {
-            ft.transform(a[i]);   
+        for (int i = 0; i < data.length; i++) {
+            f.transform(data[i]);
         }
-        
-        
+
         //Spaltentransformation
-        Complex[] col = new Complex[a.length];
-        for (int i = 0; i < a[0].length; i++) {
-            for (int j = 0; j < a.length; j++) {
-                col[j] = a[j][i];    
-            }    
-            
-            ft.transform(col);
-            
-            for (int j = 0; j < a.length; j++) {
-                a[j][i] = col[j];    
-            }            
-        }        
-     }
-          
+        Complex[] col = new Complex[data.length];
+        for (int i = 0; i < data[0].length; i++) {
+            for (int j = 0; j < data.length; j++) {
+                col[j] = data[j][i];
+            }
+
+            f.transform(col);
+
+            for (int j = 0; j < data.length; j++) {
+                data[j][i] = col[j];
+            }
+        }
+    }
+
 }
