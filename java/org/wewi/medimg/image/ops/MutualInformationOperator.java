609df56f7b4b310fb46b1ce451576140d6d0b7b6
cvsuser <>
2002-12-23 13:57:38 +0000

    *** empty log message ***

---
+++ java/org/wewi/medimg/image/ops/MutualInformationOperator.java
@@ -0,0 +1,28 @@
+/**
+ * MutualInformationOperator.java
+ * 
+ * Created on 23.12.2002, 14:56:12
+ *
+ */
+package org.wewi.medimg.image.ops;
+
+/**
+ * @author Franz Wilhelmstötter
+ * @version 0.1
+ */
+public class MutualInformationOperator implements BinaryOperator {
+
+    /**
+     * Constructor for MutualInformationOperator.
+     */
+    public MutualInformationOperator() {
+        super();
+    }
+
+    /**
+     * @see org.wewi.medimg.image.ops.BinaryOperator#process(int, int)
+     */
+    public void process(int color1, int color2) {
+    }
+
+}
