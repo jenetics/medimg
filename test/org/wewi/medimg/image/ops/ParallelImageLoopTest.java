a6b33a7899975d8f41cb3b25bf4c6460509a204a
Franz Wilhelmstötter <franz.wilhelmstoetter@gmail.com>
2003-02-18 14:30:56 +0000

    *** empty log message ***

---
+++ test/org/wewi/medimg/image/ops/ParallelImageLoopTest.java
@@ -0,0 +1,42 @@
+/**
+ * ParallelImageLoopTest.java
+ * 
+ * Created on 18.02.2003, 15:27:25
+ *
+ */
+package org.wewi.medimg.image.ops;
+
+import junit.framework.TestCase;
+
+/**
+ * @author Franz Wilhelmstötter
+ * @version 0.1
+ */
+public class ParallelImageLoopTest extends TestCase {
+
+	/**
+	 * Constructor for ParallelImageLoopTest.
+	 * @param arg0
+	 */
+	public ParallelImageLoopTest(String arg0) {
+		super(arg0);
+	}
+
+	/**
+	 * @see TestCase#setUp()
+	 */
+	protected void setUp() throws Exception {
+		super.setUp();
+	}
+
+	/**
+	 * @see TestCase#tearDown()
+	 */
+	protected void tearDown() throws Exception {
+		super.tearDown();
+	}
+    
+    public void testLoop() {
+    }
+
+}
