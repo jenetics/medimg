863df7e1f9626d28056c20cc61b342f54051c74b
Franz Wilhelmstötter <franz.wilhelmstoetter@gmail.com>
2003-02-18 14:50:10 +0000

    *** empty log message ***

--- java/org/wewi/medimg/util/Timer.java
+++ java/org/wewi/medimg/util/Timer.java
@@ -1,4 +1,4 @@
-/*
+/**
  * Timer.java
  *
  * Created on 17. Jänner 2002, 19:42
@@ -18,39 +18,66 @@ public final class Timer {
     private long startTime;
     private long stopTime;
     
+    
     public Timer() {
         this("");    
     }
 
+    /**
+     * Constructs a new <code>Timer</code> object with the
+     * specified name.
+     * 
+     * @param name name of the timer.
+     */
     public Timer(String name) {
         timerName = name;
         startTime = System.currentTimeMillis();
         stopTime = startTime;
     }
     
+    /**
+     * Starts the <code>Timer</code>
+     */
     public void start() {
         startTime = System.currentTimeMillis();
     }
     
+    /**
+     * Stops the <code>Timer</code>
+     */
     public void stop() {
         stopTime = System.currentTimeMillis();
     }
     
+    /**
+     * Returns the duration between starting and stopping the <code>Timer</code>
+     * 
+     * @return long duration between start and stop.
+     */
     public long duration() {
         return stopTime - startTime;
     }
     
+    /**
+     * Prints the duration time to the given <code>PrintStream</code>
+     * 
+     * @param out the duration time is printed to this <code>PrintStream</code>
+     */
     public void print(PrintStream out) {       
         out.println(toString());
     }
     
+    /**
+     * Prints the duration time to the standard output stream.
+     * This is equivalent to
+     * <pre>
+     *     print(System.out);
+     * </pre>
+     */
     public void print() {
         print(System.out);
     }
     
-    public void printSplitTime(String name) {
-    	
-    }
     
     
     public String toString() {
