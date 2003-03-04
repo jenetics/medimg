18af698b540fb71821fc918b3b55e0ba212ce5df
Franz Wilhelmstötter <franz.wilhelmstoetter@gmail.com>
2003-03-04 19:44:38 +0000

    *** empty log message ***

---
+++ java/org/wewi/medimg/viewer/image/ImageGrid.java
@@ -0,0 +1,26 @@
+/**
+ * ImageGrid.java
+ * 
+ * Created on 04.03.2003, 20:19:48
+ *
+ */
+package org.wewi.medimg.viewer.image;
+
+import java.awt.Graphics;
+
+
+/**
+ * @author Franz Wilhelmstötter
+ * @version 0.1
+ */
+public class ImageGrid implements ImageCanvas {
+    
+    
+	/**
+	 * @see org.wewi.medimg.viewer.image.ImageCanvas#draw(java.awt.Graphics, org.wewi.medimg.viewer.image.ImagePanel)
+	 */
+	public void draw(Graphics graph, ImagePanel panel) {
+	}
+
+
+}
