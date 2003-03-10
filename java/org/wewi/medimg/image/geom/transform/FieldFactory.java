931d53683e14275e4e072031f10ff7653d0fa451
fwilhelm <>
2003-03-10 09:07:55 +0000

    *** empty log message ***

---
+++ java/org/wewi/medimg/image/geom/transform/FieldFactory.java
@@ -0,0 +1,23 @@
+/**
+ * FieldFactory.java
+ * 
+ * Created on 10.03.2003, 10:06:32
+ *
+ */
+package org.wewi.medimg.image.geom.transform;
+
+/**
+ * @author Franz Wilhelmstötter
+ * @version 0.1
+ */
+public final class FieldFactory {
+
+    private FieldFactory(){
+    }
+    
+    
+    public static RegularDisplacementField createRegularField() {
+        return null;
+    }
+    
+}
