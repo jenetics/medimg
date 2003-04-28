8eb0666db2ac7bba1f158790665a90648543bb7d
fwilhelm <>
2003-04-28 13:33:20 +0000

    *** empty log message ***

---
+++ java/org/wewi/medimg/math/MutableNumber.java
@@ -0,0 +1,29 @@
+/**
+ * MutableNumber.java
+ * Created on 28.04.2003
+ *
+ */
+package org.wewi.medimg.math;
+
+import org.wewi.medimg.util.Mutable;
+
+/**
+ * @author Franz Wilhelmstötter
+ * @version 0.1
+ */
+public abstract class MutableNumber extends Number implements Mutable, Cloneable {
+
+    public abstract void setValue(byte n);
+    
+    public abstract void setValue(short n);
+
+    public abstract void setValue(int n);
+    
+    public abstract void setValue(long n);
+
+    public abstract void setValue(float n);
+    
+    public abstract void setValue(double n);
+
+    public abstract Object clone();
+}
